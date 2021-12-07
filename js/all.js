89a6ac004a1059dbaf0cb1666bcc2c71bb86a615
YiFanHuang0115 <95221343+YiFanHuang0115@users.noreply.github.com>
2021-12-07 15:10:29 +0800

    ver1.8
    照老師建議，全部修改完畢。

--- js/all.js
+++ js/all.js
@@ -1,38 +1,37 @@
-$(document).ready(function(){
-    $('.timeline p, .timeline2 p').hide();
-    $(".timeline li:nth-child(1)").click(function(event){
-        $('#dialog_1').dialog({width: 640});
-    });
-    $(".timeline li:nth-child(2)").click(function(event){
-        $('#dialog_2').dialog({width: 640});
-    });
-    $(".timeline li:nth-child(3)").click(function(event){
-        $('#dialog_3').dialog({width: 640});
-    });
-    $(".timeline li:nth-child(4)").click(function(event){
-        $('#dialog_4').dialog({width: 640});
-    });
-    $(".timeline2 li:nth-child(1)").click(function(event){
-        $('#dialog_5').dialog({width: 375});
-    });
-    $(".timeline2 li:nth-child(2)").click(function(event){
-        $('#dialog_6').dialog({width: 375});
-    });
-    $(".timeline2 li:nth-child(3)").click(function(event){
-        $('#dialog_7').dialog({width: 375});
-    });
-    $(".timeline2 li:nth-child(4)").click(function(event){
-        $('#dialog_8').dialog({width: 375});
-    });
+// $(document).ready(function(){
+//     $('.timeline p, .timeline2 p').hide();
+//     $(".timeline li:nth-child(1)").click(function(event){
+//         $('#dialog_1').dialog({width: 640});
+//     });
+//     $(".timeline li:nth-child(2)").click(function(event){
+//         $('#dialog_2').dialog({width: 640});
+//     });
+//     $(".timeline li:nth-child(3)").click(function(event){
+//         $('#dialog_3').dialog({width: 640});
+//     });
+//     $(".timeline li:nth-child(4)").click(function(event){
+//         $('#dialog_4').dialog({width: 640});
+//     });
+//     $(".timeline2 li:nth-child(1)").click(function(event){
+//         $('#dialog_5').dialog({width: 375});
+//     });
+//     $(".timeline2 li:nth-child(2)").click(function(event){
+//         $('#dialog_6').dialog({width: 375});
+//     });
+//     $(".timeline2 li:nth-child(3)").click(function(event){
+//         $('#dialog_7').dialog({width: 375});
+//     });
+//     $(".timeline2 li:nth-child(4)").click(function(event){
+//         $('#dialog_8').dialog({width: 375});
+//     });
 
-})
+// })
 
 
 
 const arrswitch = document.querySelector('#switch');
 let classList = arrswitch.classList;
 let autobiography = document.querySelector(".autobiography p");
-
 arrswitch.addEventListener('click', function(){
     const result = classList.toggle(1);
     if(result){
